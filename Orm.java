720e8a9144d7e00cc03c466acf2afe777cc444e2
frfr1701 <fredrik.frolund@yh.nackademin.se>
2017-10-02 10:01:55 +0200

    gjorde om orm

--- Orm.java
+++ Orm.java
@@ -3,11 +3,15 @@ package sprint1;
 import javax.swing.JOptionPane;
 
 public class Orm extends Djur {
+    private static final double ORMMAT=20;
     public Orm(String namn,double vikt) {
         super(namn,vikt);
     }
+    public double hurMycketMat(){
+        return ORMMAT;
+    }
         @Override
     public void printMåltid() {
-        JOptionPane.showMessageDialog(null, getNamn() + " är en orm som väger " + (int) getVikt() + "kg, den behöver 20g ormpellets");
+        JOptionPane.showMessageDialog(null, getNamn() + " är en orm som väger " + (int) getVikt() + "kg, den behöver " + hurMycketMat() +"g ormpellets");
     }
 }
